31f30773203dd31be61402dbe0bc8d7b58e493ca
ixi <125541247+indixiaj@users.noreply.github.com>
2024-03-06 14:19:29 +0000

    Update 5_Settings_and_Help.py

--- NewAgriAid/pages/5_Settings_and_Help.py
+++ NewAgriAid/pages/5_Settings_and_Help.py
@@ -74,4 +74,4 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
-local_css("NewAgriAid\pages\style.css")
+local_css("Agri-Aid-App\NewAgriAid\pages\style.css")
