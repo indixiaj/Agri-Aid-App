e872b01c0c7ddcf4f2a9019e794f548bcc1cce4b
ixi <125541247+indixiaj@users.noreply.github.com>
2024-03-06 13:59:28 +0000

    test form format

--- NewAgriAid/pages/5_Settings_and_Help.py
+++ NewAgriAid/pages/5_Settings_and_Help.py
@@ -74,4 +74,4 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
-local_css("NewAgriAid\pages\style\style.css")
+local_css("pages\style\style.css")
