519130f274383ac05ea40600423b466e4d2f8825
ixi <125541247+indixiaj@users.noreply.github.com>
2024-04-23 10:59:44 +0100

    Update 3_Soil_Temperature.py

--- NewAgriAid/pages/3_Soil_Temperature.py
+++ NewAgriAid/pages/3_Soil_Temperature.py
@@ -21,8 +21,8 @@ st.set_page_config(page_title="Soil Temperature", page_icon="🌡️", layout='w
 # Database configuration
 db_config = {
     'user': 'root',
-    'password': 'agriaid321',
-    'host': '34.147.193.127',
+    'password': 'ES410_Project',
+    'host': '34.34.136.92',
     'database': 'mydb'
 }
 # Connect to the database
